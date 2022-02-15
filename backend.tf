b0c3adceef609603d68de7b58bdcc18a5200cd9f
Balasubramanian Ramachandran <balasubramanianramachandran@rfny133.local>
2022-02-14 20:02:07 -0600

    adding backend file for testing

--- backend.tf
+++ backend.tf
@@ -1,6 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = "mystatefiles-testing/Terraform/terraform.tfstate"
-    region = "us-east-1"
+    bucket  = "mystatefiles-testing"
+    key     = "terraform.tfstate"
+    region  = "us-east-1"
+    encrypt = true
   }
 }
